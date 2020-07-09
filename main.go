34f7157defbee315e97e84e02e81e46dd9794a4d
TranBaNgoc <1612431@student.hcmus.edu.vn>
2020-07-09 17:21:58 +0700

    update notify

--- main.go
+++ main.go
@@ -17,21 +17,26 @@ func main() {
 	InitWorkers(ct, wg, int64(PushConf.WorkerNum), int64(PushConf.QueueNum))
 
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8899"
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/follow", FollowNotify)
+	mux.HandleFunc("/live", LiveNotify)
+
 	fmt.Println("Server listening on port", port)
 	log.Fatalln(http.ListenAndServe(":"+port, mux))
 }
 
-func FollowNotify(writer http.ResponseWriter, request *http.Request) {
+func LiveNotify(writer http.ResponseWriter, request *http.Request) {
 	user_id, ok := request.URL.Query()["user_id"]
 	if !ok {
 		log.Println("user_id is not param query")
 	} else {
 		req := PushNotification{}
 		req.Tokens = GetUserTokens(user_id[0])
-		req.Message = "This is a follow"
+		req.Message = user_id[0] + " is start living"
 		req.Platform = 2
 		success := PushToAndroid(req)
 		if success {
@@ -40,9 +45,29 @@ func FollowNotify(writer http.ResponseWriter, request *http.Request) {
 			writer.Write([]byte("Send message fail"))
 			return
 		}
-		log.Println("Send message success")
 		writer.WriteHeader(200)
 		writer.Write([]byte("{ \"message\": \"Send message success\"}"))
+	}
+}
 
+func FollowNotify(writer http.ResponseWriter, request *http.Request) {
+	user_id, ok := request.URL.Query()["user_id"]
+	f_id, ok := request.URL.Query()["f_id"]
+	if !ok {
+		log.Println("user_id is not param query")
+	} else {
+		req := PushNotification{}
+		req.Tokens = GetUserTokens(user_id[0])
+		req.Message = f_id[0] + "following you"
+		req.Platform = 2
+		success := PushToAndroid(req)
+		if success {
+			log.Println("Send message fail")
+			writer.WriteHeader(400)
+			writer.Write([]byte("Send message fail"))
+			return
+		}
+		writer.WriteHeader(200)
+		writer.Write([]byte("{ \"message\": \"Send message success\"}"))
 	}
 }
--- model.go
+++ model.go
@@ -6,8 +6,6 @@ import (
 	"log"
 )
 
-
-
 func GetUserTokens(id string) []string {
 	db, err := sql.Open("mysql", "5EIxJdtsCE:mX0MKS9YYG@tcp(remotemysql.com:3306)/5EIxJdtsCE")
 	if err != nil {
@@ -17,6 +15,26 @@ func GetUserTokens(id string) []string {
 
 	tokens := []string{}
 	results, err := db.Query("select device_token from user where device_token != \"\" and device_token is not null and id = " + id)
+	for results.Next() {
+		var token string
+		err = results.Scan(&token)
+		if err != nil {
+			log.Println(err)
+		}
+		tokens = append(tokens, token)
+	}
+	return tokens
+}
+
+func GetFollowUserTokens(user_id string) []string {
+	db, err := sql.Open("mysql", "5EIxJdtsCE:mX0MKS9YYG@tcp(remotemysql.com:3306)/5EIxJdtsCE")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	tokens := []string{}
+	results, err := db.Query("select u.device_token from user u join user_folower uf on u.id = uf.user_id where uf.state = 1 and u.device_token != \"\" and u.device_token is not null and id = " + user_id)
 	for results.Next() {
 		var token string
 		err = results.Scan(&token)
